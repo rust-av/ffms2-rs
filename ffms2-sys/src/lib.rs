6f68d53b9ec94d53e4e0c3085809e0f92c2290a5
Christopher Gundler <christopher@gundler.de>
2021-04-22 11:31:54 +0200

    Allow non-compliant names in the ffms2-sys crate

--- ffms2-sys/src/lib.rs
+++ ffms2-sys/src/lib.rs
@@ -2,6 +2,7 @@
 #![allow(non_camel_case_types)]
 #![allow(non_snake_case)]
 #![allow(non_upper_case_globals)]
+#![allow(clippy::upper_case_acronyms)]
 
 include!(concat!(env!("OUT_DIR"), "/ffms2.rs"));
 
